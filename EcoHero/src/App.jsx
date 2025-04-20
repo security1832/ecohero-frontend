b1d6d03bf5042b591b5ea229d7eaeac6ad5af207
security1832 <bravinhann@gmail.com>
2025-04-20 17:58:56 +0300

    Initial project setup

--- EcoHero/src/App.jsx
+++ EcoHero/src/App.jsx
@@ -1,11 +1,4 @@
-import './index.css';
-
 function App() {
-  return (
-    <div className="bg-green-100 min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-green-700">Welcome to EcoHero!</h1>
-    </div>
-  );
+  return <div className="text-center">EcoHero</div>;
 }
-
 export default App;
